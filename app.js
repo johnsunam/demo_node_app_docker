22b14f5c09b3312d367fcd959b48dd06164c369e
johnsunam <sunamjohn@gmail.com>
2020-05-19 09:01:04 +0545

    code added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = process.env.APP_PORT;
 router.use(function(req, res, next) {
   console.log('/' + req.method);
   next();
